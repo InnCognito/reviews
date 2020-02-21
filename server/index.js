f58177adf513de89fc76a6bea53dc9efa3ca7c36
7FFF00 <t42please@gmail.com>
2020-02-21 11:35:17 -0800

    REFACTOR to call from postgres controller, return desired data shape

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.static(`${__dirname}/../client/dist`));
 app.get('/reviews/:room_id', (req, res) => {
   const target = {room_id: req.params.room_id};
   db.read(target)
-    .then((data) => res.status(200).send(data))
+    .then((data) => res.status(200).send([{ reviews: data, room_id: target }]))
     .catch((err) => res.status(500).send('ERROR RETRIEVING DATA'));
 });
 
@@ -22,3 +22,6 @@ app.listen(PORT, ()=>{
   console.log("Server is now listening on port:", PORT);
   console.log(`Visit website at http://localhost:${PORT}/:id=1`);
 });
+
+// Desired Data Shape on front end
+// [0] => { review: [<reviews>], room_id: <room_id> }
